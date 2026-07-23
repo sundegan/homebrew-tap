3e1365e6a261df553f1719cd6248acd8ed10fb8d
Degan Sun <sundegan1998@gmail.com>
2026-07-24 00:58:05 +0800

    macicon 1.0.2

--- Formula/macicon.rb
+++ Formula/macicon.rb
@@ -5,11 +5,11 @@ class Macicon < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/sundegan/macos-icon-generator/releases/download/v1.0.1/macicon-aarch64-apple-darwin.tar.gz"
-      sha256 "83b439b725f4cad9576b8cad09b6322fe0497d2bb1be5b05e69dc65c52b271d7"
+      url "https://github.com/sundegan/macos-icon-generator/releases/download/v1.0.2/macicon-aarch64-apple-darwin.tar.gz"
+      sha256 "a8e08a9c58db11d44f6f30dd4bf9c50683b9606f99bc27cdaf4b671cf7e1a896"
     else
-      url "https://github.com/sundegan/macos-icon-generator/releases/download/v1.0.1/macicon-x86_64-apple-darwin.tar.gz"
-      sha256 "c2659b4491808ee3d06cbb74c4a4440b1ccc6d6c400d4aaa61943dc58c9dfcb3"
+      url "https://github.com/sundegan/macos-icon-generator/releases/download/v1.0.2/macicon-x86_64-apple-darwin.tar.gz"
+      sha256 "a5c70ff0be457b39c53d1a481d8e87f234bbc3249f70c3eef3afbbb885eb27fc"
     end
   end
 
