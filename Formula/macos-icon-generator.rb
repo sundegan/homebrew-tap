f82d2a9a737c153f051d0f1abec775f755a9f675
Degan Sun <sundegan1998@gmail.com>
2026-07-23 17:37:39 +0800

    chore: update macos icon generator formula to 0.1.1

--- Formula/macos-icon-generator.rb
+++ Formula/macos-icon-generator.rb
@@ -1,26 +1,26 @@
 class MacosIconGenerator < Formula
   desc "Generate macOS-style icons from square SVG, PNG, or JPEG images"
   homepage "https://github.com/sundegan/macos-icon-generator"
-  version "0.1.0"
+  version "0.1.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/sundegan/macos-icon-generator/releases/download/v#{version}/macos-icon-generator-v#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "7f2c6bf2e1867ac2af00b44984493a6d2b16737c31a286826f15738ac9729abf"
+      sha256 "0ed8bc6e9fb67362e28230158819bf0de98da5b9376f9a697e53abf35d01e5b0"
     else
       url "https://github.com/sundegan/macos-icon-generator/releases/download/v#{version}/macos-icon-generator-v#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "447af65fc251a7349455480c35f7b11574e16abd69763217c00174a5cea7351e"
+      sha256 "faf6d554f7dc23dd2e4b7241ba1584691a8566bbc1ec4d2bfa2d51d2be6f39f0"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/sundegan/macos-icon-generator/releases/download/v#{version}/macos-icon-generator-v#{version}-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "0ee7e9ca95fa5cccc265f1fc8900fc8d01e77b32ca18edf34d43710340b530a9"
+      sha256 "ab08ea75dc0ceb913a526b198510fdd9e50b7813b9fa901b1b2b5a000968418f"
     else
       url "https://github.com/sundegan/macos-icon-generator/releases/download/v#{version}/macos-icon-generator-v#{version}-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "641571c4ba8edd674c2d9a3c08ca9baa4a0392493cefe9f3671dd9b8903e709e"
+      sha256 "dd4ae32f491f83547800dfbeab03575a54d4f87762d5e7bdd928f97d3dc0d821"
     end
   end
 
